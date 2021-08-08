e138ce8bab85d638feec9c8571cfc0a0fc745b43
Alex Derby <n01037933@unf.edu>
2021-08-08 15:54:43 -0400

    changed controller

--- PreSemesterProject/Controllers/OpportunitiesController.cs
+++ PreSemesterProject/Controllers/OpportunitiesController.cs
@@ -10,7 +10,7 @@ namespace PreSemesterProject.Controllers
     {
         public IActionResult Index()
         {
-            return Ok("Opportunities");
+            return Ok("Test");
         }
     }
 }
