550ebbb1216277c5ba22a70108b52267167128b9
gaskillcw <gaskillcw@gmail.com>
2021-08-16 12:01:33 -0400

    Copied Opportunities view to newly created Volunteer folder

--- PreSemesterProject/Controllers/VolunteersController.cs
+++ PreSemesterProject/Controllers/VolunteersController.cs
@@ -10,7 +10,23 @@ namespace PreSemesterProject.Controllers
     {
         public IActionResult Index()
         {
-            return Ok("Volunteers");
+            //return Ok("Volunteers");
+            return View("~/Views/Volunteers/Volunteers.cshtml");
+        }
+
+        public IActionResult Filters()
+        {
+            return View("~/Views/Volunteers/VolunteersFilters.cshtml");
+        }
+
+        public IActionResult Edit()
+        {
+            return View("~/Views/Volunteers/VolunteersEdit.cshtml");
+        }
+
+        public IActionResult Add()
+        {
+            return View("~/Views/Volunteers/VolunteersAdd.cshtml");
         }
     }
 }
